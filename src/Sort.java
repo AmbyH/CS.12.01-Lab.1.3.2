c87d4217417d4fcfe4ed994eff60683c9e35cc3d
AmbyH <kinghah2026@student.cis.edu.hk>
2024-09-30 09:00:44 +0800

    v0

--- src/Sort.java
+++ src/Sort.java
@@ -1,41 +1,91 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Sort {
 
     public static int[] bubbleSort(int[] array) {
-
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
         return array;
-
     }
 
     public static String[] bubbleSort(String[] array) {
-
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j].compareToIgnoreCase(array[j + 1]) > 0) {
+                    String temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
         return array;
-
     }
 
     public static ArrayList<Integer> bubbleSort(ArrayList<Integer> list) {
-
+        int n = list.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    Collections.swap(list, j, j + 1);
+                }
+            }
+        }
         return list;
-
     }
 
     public static int[] selectionSort(int[] array) {
-
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
         return array;
-
     }
 
     public static String[] selectionSort(String[] array) {
-
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j].compareToIgnoreCase(array[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+            String temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
         return array;
-
     }
 
     public static ArrayList<String> selectionSort(ArrayList<String> list) {
-
+        int n = list.size();
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (list.get(j).compareToIgnoreCase(list.get(minIndex)) < 0) {
+                    minIndex = j;
+                }
+            }
+            Collections.swap(list, i, minIndex);
+        }
         return list;
-
     }
-
 }
